b36ad3331f84c22d19ce43ebf3fadff8073c6005
Zolotarev Kirill <kzolotarev@ozon.ru>
2019-11-25 16:22:12 +0300

    Начал делать page object

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
     private WebDriver driver;
@@ -14,4 +17,10 @@ public class MainPage {
     private By product_title = By.cssSelector(".n-snippet-card2__title > a.link");
     private By search = By.id("header-search");
     private By search_btn = By.cssSelector(".button2");
+
+
+    private void waitForVisibility(WebElement element) throws Error{
+        new WebDriverWait(driver, 60)
+                .until(ExpectedConditions.visibilityOf(element));
+    }
 }
